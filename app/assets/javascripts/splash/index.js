ae9c1ce29d10f1665ad1921cebc02b93d6fb0db7
Juan M Berrocal <juanmaberrocal37@gmail.com>
2016-11-13 15:02:39 -0500

    splash index page updated
    updated styles
    
    added images
    
    added fonts
    
    added description and text blocks to sections

--- app/assets/javascripts/splash/index.js
+++ app/assets/javascripts/splash/index.js
@@ -1,5 +1,14 @@
 angular.module("wkndrCr")
 	.controller('wkndrSplash', ["$scope", "bgChangeService", function($scope, bgChangeService){
+		
+		// click function to expand/collapse jumbo text container
+		$scope.expandCollapseJumbo = function(ev){
+			var $this = $(ev.currentTarget),
+				container = $this.parent();
+			// toggle collapse
+			container.toggleClass("collapsed");
+		}
+
 		var bgChange; // keep track of body bg change interval
 	    bgChange = bgChangeService.startBgChange({ div:$("div.jumbotron") });
 
