927adc0e9baa6c63e3a7133868d7337b22597706
Juan M Berrocal <juanmaberrocal37@gmail.com>
2016-11-15 12:00:52 -0500

    added facebook oauth
    added configurations for facebook login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 WkndrCr::Application.routes.draw do
 	# Devise authentication API routes
-	mount_devise_token_auth_for 'User', at: 'auth'
+	mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+		omniauth_callbacks: 'overrides/omniauth_callbacks'
+	}
 
 	# Splash page routed as root
 	root 'home#index'
