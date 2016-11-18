f125dd4fa5f89483b58f2652249ab38b087bd449
Juan M Berrocal <juanmaberrocal37@gmail.com>
2016-11-18 11:59:02 -0500

    added specs for location requests
    added tests to ensure users can access locations
    
    added tests to ensure error response is in expected format

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,6 +1,8 @@
 module Api
 	module V1
 		class LocationsController < ApplicationController
+			include ControllerErrorResponseHelper
+
 			# ensure user is logged in
 			before_action :authenticate_user!
 
@@ -10,51 +12,77 @@ module Api
 			# default to json response
 			respond_to :json
 
+			# GET /locations.json
 			def index
 				begin
 					# return response as json array of locations
 					render json: Location.all.as_json
 				rescue => e
 					# catch errors and return message
-					render json: { errors: Array(e.message) }, status: :unprocessable_entity
+					build_error_response(e.message)
 				end
 			end
 
+			# GET /locations/1.json
 			def show
 				begin
 					# return response as json object of location
 					render json: @location.as_json
 				rescue => e
 					# catch errors and return message
-					render json: { errors: Array(e.message) }, status: :unprocessable_entity
+					build_error_response(e.message)
 				end
 			end
 
-			# todo: change to begin|rescue block for controlled json response
+			# POST /locations.json
 			def create
-				# respond_with handles errors in create fail
-				respond_with :api, :v1, Location.create(location_params)
+				begin
+					# initialize new location
+					@location = Location.new(location_params)
+
+					# save/create (catch validation errors)
+					if @location.save
+						# return response as json object of created location
+						render json: @location.as_json 
+					else
+						build_error_response(@location.errors.full_messages)
+					end
+				rescue => e
+					# catch errors and return message
+					build_error_response(e.message)
+				end
 			end
 
 		  # PATCH/PUT /locations/1.json
 			def update
 				begin
+					# update (catch validation errors)
 					if @location.update(location_params)
 						# return response as json object of updated location
 						render json: @location.as_json 
 					else
-						render json: { errors: @location.errors.full_messages }, status: :unprocessable_entity
+						build_error_response(@location.errors.full_messages)
 					end
 				rescue => e
 					# catch errors and return message
-					render json: { errors: Array(e.message) }, status: :unprocessable_entity
+					build_error_response(e.message)
 				end
 			end
 
-			# todo: change to begin|rescue block for controlled json response
+			# DELETE /logactions/1.json
 			def destroy
-				# respond_with handles errors in destroy fail
-				respond_with @location.destroy
+				begin
+					# update (catch validation errors)
+					if @location.destroy
+						# return response as json object of updated location
+						render json: :no_head
+					else
+						build_error_response(@location.errors.full_messages)
+					end
+				rescue => e
+					# catch errors and return message
+					build_error_response(e.message)
+				end
 			end
 
 			private
